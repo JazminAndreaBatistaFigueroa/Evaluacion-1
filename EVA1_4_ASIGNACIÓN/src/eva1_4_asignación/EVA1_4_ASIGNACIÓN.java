181b3cdfbe7844c84aec46449bf67334749ff8e4
JazminAndreaBatistaFigueroa <159494335+JazminAndreaBatistaFigueroa@users.noreply.github.com>
2024-02-15 20:44:15 -0700

    Add files via upload

---
+++ EVA1_4_ASIGNACIÓN/src/eva1_4_asignación/EVA1_4_ASIGNACIÓN.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_asignación;
+
+/**
+ *
+ * @author jazzb
+ */
+public class EVA1_4_ASIGNACIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad;//DELACARACIÓN
+   edad = 18; //ASIGNACIÓN -- UN SOLO = SIGNIFICA ASIGNAR
+   System.out.println("Tu edad es:");
+  System.out.println(edad);
+   
+  String Nombre;
+  Nombre = "Jazmin Andrea Batista Figueroa";
+  System.out.println("Tu nombre es:");
+  System.out.println(Nombre);
+  
+  String Carrera;
+  Carrera = "Ingenieria en Sistemas Computaciones";
+  System.out.println("Tu carrera es:");
+  System.out.println(Carrera);
+   
+    }
+    
+}
